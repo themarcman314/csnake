429470b20c8179c9133f026c0190bb30b11fd800
Marc Robison <marc@marcrobison.com>
2026-02-27 15:33:28 +0100

    feat: set board size

--- src/game.c
+++ src/game.c
@@ -49,7 +49,6 @@ void game_init(Game *g) {
 	term_init();
 	g->state = STATE_GAME_WELCOME;
 	g->tick_speed = 1000.0 / TICK_FREQUENCY;
-	g->b = board_create(BOARD_WIDTH, BOARD_HEIGHT);
 }
 
 GameState game_end(Game *const g) {
@@ -160,8 +159,11 @@ GameState game_welcome(TermInputKey key) {
 }
 
 GameState game_configure(Game *g) {
-
+	static bool configured_board_height = false;
+	static bool configured_board_width = false;
 	static float freq = TICK_FREQUENCY;
+	static int width = BOARD_WIDTH;
+	static int height = BOARD_HEIGHT;
 	static int last_tick = 0;
 	int now = millis();
 	if (now - last_tick >= 50) {
@@ -174,35 +176,76 @@ GameState game_configure(Game *g) {
 		printf("============== csnake ==============\n");
 		term_color_clear();
 		printf("\033[%d;%dH", ++offset_rows, offset_colums);
-		printf("   Set snake speed with ");
-		term_color_set(CYN);
-		putchar('+');
-		term_color_clear();
-		putchar('/');
-		term_color_set(CYN);
-		putchar('-');
-		term_color_clear();
-		printf(" keys\n");
-		printf("\033[%d;%dH", ++offset_rows, offset_colums);
-		printf("Snake speed: %s%.1f%s (tiles per second)\n", MAG, freq, COLOR_RESET);
-		printf("\033[%d;%dH", ++offset_rows, offset_colums);
-		printf("     Quit with '%sq%s' at any time\n", CYN, COLOR_RESET);
+		if (configured_board_height && configured_board_width) {
+			printf("   Set snake speed with %s+%s/%s-%s keys\n",
+			       CYN, COLOR_RESET, CYN, COLOR_RESET);
+			printf("\033[%d;%dH", ++offset_rows, offset_colums);
+			printf("  Snake speed: %s%.1f%s tiles per second\n", MAG,
+			       freq, COLOR_RESET);
+			++offset_rows;
+			printf("\033[%d;%dH", ++offset_rows, offset_colums);
+			printf("     Quit with '%sq%s' at any time\n", CYN,
+			       COLOR_RESET);
+		} else if (configured_board_width) {
+			printf("   Set snake speed with %s+%s/%s-%s keys\n",
+			       CYN, COLOR_RESET, CYN, COLOR_RESET);
+			printf("\033[%d;%dH", ++offset_rows, offset_colums);
+			printf("      Board height: %s%d%s tiles\n", MAG, height,
+			       COLOR_RESET);
+			++offset_rows;
+			printf("\033[%d;%dH", ++offset_rows, offset_colums);
+			printf("     Quit with '%sq%s' at any time\n", CYN,
+			       COLOR_RESET);
+
+		} else {
+			printf("   Set snake speed with %s+%s/%s-%s keys\n",
+			       CYN, COLOR_RESET, CYN, COLOR_RESET);
+			printf("\033[%d;%dH", ++offset_rows, offset_colums);
+			printf("      Board height: %s%d%s tiles\n", MAG, width,
+			       COLOR_RESET);
+			++offset_rows;
+			printf("\033[%d;%dH", ++offset_rows, offset_colums);
+			printf("     Quit with '%sq%s' at any time\n", CYN,
+			       COLOR_RESET);
+		}
 		printf("\033[%d;%dH", ++offset_rows, offset_colums);
 		term_color_set(GRN);
 		printf("====================================\n");
 		term_color_clear();
 	}
-	float const delta = 0.1; // +- 0.1 Hz
 
+	float const delta = 0.1; // +- 0.1 Hz
 	switch (g->key) {
 	case IN_ENTER:
-		g->tick_speed = 1000.0F / freq;
-		return STATE_GAME_RUN;
+		if (configured_board_height && configured_board_width) {
+			g->tick_speed = 1000.0F / freq;
+			g->b = board_create(width, height);
+			return STATE_GAME_RUN;
+		} else if (configured_board_width) {
+			configured_board_height = true;
+		} else {
+			configured_board_width = true;
+		}
+		break;
 	case IN_PLUS:
-		freq += delta;
+		if (configured_board_height && configured_board_width) {
+			freq += delta;
+		} else if (configured_board_width) {
+			height++;
+		} else {
+			width++;
+		}
 		break;
 	case IN_MINUS:
-		freq -= delta;
+		if (width == 0 || height == 0 || freq == 0)
+			break;
+		if (configured_board_height && configured_board_width) {
+			freq -= delta;
+		} else if (configured_board_width) {
+			height--;
+		} else {
+			width--;
+		}
 		break;
 	default:
 		break;
@@ -212,6 +255,7 @@ GameState game_configure(Game *g) {
 
 void game_fsm_run(void) {
 	Game g;
+	g.b = NULL;
 	game_init(&g);
 	while (g.state != STATE_GAME_EXIT) {
 		term_init();
@@ -234,9 +278,11 @@ void game_fsm_run(void) {
 			g.state = game_configure(&g);
 			break;
 		case STATE_GAME_EXIT:
-			food_destroy(&g.b->f);
-			snake_kill(&g.b->s);
-			board_destroy(&g.b);
+			if (g.b) {
+				food_destroy(&g.b->f);
+				snake_kill(&g.b->s);
+				board_destroy(&g.b);
+			}
 
 		default:
 			g.state = STATE_GAME_EXIT;
